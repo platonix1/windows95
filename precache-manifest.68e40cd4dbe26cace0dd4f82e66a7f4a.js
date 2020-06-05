ca719d0c5928cd15ca3307312d0128a7f0029e15
platonix1 <danplaton88@gmail.com>
2020-06-05 12:06:24 +0300

    last version

---
+++ precache-manifest.68e40cd4dbe26cace0dd4f82e66a7f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7443297dbeecf2aab52dfda98b7004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a1bf36a5cea015646e",
+    "url": "/static/css/main.9d081b65.chunk.css"
+  },
+  {
+    "revision": "f23c9a614dff0fdff31f",
+    "url": "/static/js/2.14704b8f.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/static/js/2.14704b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a1bf36a5cea015646e",
+    "url": "/static/js/main.142aa8c7.chunk.js"
+  },
+  {
+    "revision": "bb216cd7d1ef742c43d1",
+    "url": "/static/js/runtime-main.74984116.js"
+  }
+]);
